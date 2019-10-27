f13f88c1056079fb31479048f4932847e62d2e06
hramasamy <brindhahari@yahoo.com>
2019-10-26 23:46:44 -0700

    latest changes

---
+++ kafka-master/kakfa-producer-consumer-example/src/main/java/com/evo/kafka/consumer/ConsumerCreator.java
@@ -0,0 +1,62 @@
+package com.evo.kafka.consumer;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
+import com.evo.kafka.constants.IKafkaConstants;
+import com.evo.kafka.utils.ConsumerGroupLag;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class ConsumerCreator {
+
+
+
+	public static Consumer<Object, Object> createConsumer() {
+		ConsumerRebalanceListener listener = new ConsumerRebalanceListener() {
+
+			@Override
+			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+				//System.out.println("C : , Revoked topicPartitions : {}", partitions);
+				System.out.println ("revokded topic partition") ;
+				//commitOffsets(partitionToUncommittedOffsetMap);
+			}
+
+			@Override
+			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+				for (TopicPartition tp : partitions) {
+					//OffsetAndMetadata offsetAndMetaData = consumer.committed(tp);
+					//long startOffset = offsetAndMetaData != null ? offsetAndMetaData.offset() : -1L;
+					//System.out.println("C : {}, Assigned topicPartion : {} offset : {}", clientId, tp, startOffset);
+					System.out.println (" partition assignment");
+
+					/*if(startOffset >= 0)
+						consumer.seek(tp, startOffset);*/
+				}
+			}
+		};
+
+		final Properties props = new Properties();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, IKafkaConstants.MAX_POLL_RECORDS);
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, IKafkaConstants.OFFSET_RESET_EARLIER);
+
+		final Consumer<Object, Object> consumer = new KafkaConsumer<>(props);
+		consumer.subscribe(Collections.singletonList(IKafkaConstants.TOPIC_NAME), listener);
+		return consumer;
+	}
+
+	public static ConsumerGroupLag createLagObject () {
+		ConsumerGroupLag cLag = new ConsumerGroupLag() ;
+		cLag.setBootstrapServer(IKafkaConstants.KAFKA_BROKERS);
+		cLag.setGroup(IKafkaConstants.GROUP_ID_CONFIG);
+		return cLag ;
+	}
+
+}
--- kafka-utilities-master/ConsumerGroupLag/src/main/java/com/evo/util/KafkaUtils.java
+++ kafka-utilities-master/ConsumerGroupLag/src/main/java/com/evo/util/KafkaUtils.java
@@ -1,4 +1,73 @@
 package com.evo.util;
 
+import kafka.admin.RackAwareMode;
+import kafka.zk.AdminZkClient;
+import kafka.zk.KafkaZkClient;
+import org.apache.kafka.common.utils.Time;
+import scala.collection.Seq;
+
+import java.util.Properties;
+
 public class KafkaUtils {
+
+    String zookeeper;
+    KafkaZkClient zkClient;
+    AdminZkClient adminZkClient;
+
+    public KafkaUtils(String zk) {
+        this.zookeeper = zk;
+    }
+
+    public void connect_() {
+        Boolean isSucre = false;
+        int sessionTimeoutMs = 200000;
+        int connectionTimeoutMs = 15000;
+        int maxInFlightRequests = 1;
+        Time time = Time.SYSTEM;
+        String metricGroup = "myGroup";
+        String metricType = "myType";
+        zkClient = KafkaZkClient.apply(
+                zookeeper, isSucre, sessionTimeoutMs,
+                connectionTimeoutMs, maxInFlightRequests,
+                time, metricGroup, metricType);
+        adminZkClient = new AdminZkClient(zkClient);
+    }
+
+    public boolean topicExists (String topicName1) {
+        if (this.zkClient == null)
+            connect_();
+        return zkClient.topicExists(topicName1);
+    }
+
+    public void getAllTopics () {
+        Seq allTopic = zkClient.getAllTopicsInCluster();
+        System.out.println("Cluset has " + allTopic.length() + " topics");
+    }
+
+    public void createTopic(String topicName1) {
+
+     /*   if (topicName1 == null) {
+            throw NullPointerException() ;
+        }*/
+        int partitions = 3;
+        int replication = 1; // you should have replication factor less than or equal to number of nodes in Kafka cluster
+        Properties topicConfig = new Properties(); // you can pass topic
+
+        adminZkClient.createTopic(topicName1, partitions, replication, topicConfig, RackAwareMode.Disabled$.MODULE$);
+    }
+
+
+    public void deleteTopic (String topicName1) {
+
+        adminZkClient.deleteTopic(topicName1);
+    }
+
+    public static void main (String [] args) {
+
+        KafkaUtils ku = new KafkaUtils("localhost:2182") ;
+        ku.connect_();
+        ku.createTopic("test");
+        ku.getAllTopics();
+    }
+
 }
