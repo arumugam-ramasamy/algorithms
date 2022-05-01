0146b55022b5f4ea72473f4628863e83d889d355
Pranav K Hariharane <pranavkhariharane@Pranavs-MacBook-Air.local>
2022-05-01 09:04:33 -0700

    added changed files linkedlist

---
+++ java/interviews/src/main/java/com/evo/ib/Main.java
@@ -0,0 +1,109 @@
+package com.evo.ib;
+
+
+import com.evo.ib.linkedlist.LinkedList;
+import com.evo.ib.linkedlist.ListNode;
+
+import java.util.Random;
+
+public class Main {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2 ;
+        if (l2 == null) return l1 ;
+
+        int sum ;
+        int carry = 0 ;
+        ListNode prev = null ;
+        ListNode head = l1 ;
+        while (l1 != null && l2 != null) {
+            sum = l1.val + l2.val + carry ;
+            if (sum >= 10) {
+                carry = sum / 10 ;
+                sum = sum % 10 ;
+
+            }
+            else carry = 0 ;
+            l1.val = sum ;
+            prev = l1 ;
+            l1 = l1.next ;
+            l2 = l2.next ;
+        }
+
+        if (l1 == null && l2 != null) {
+            prev.next = l2 ;
+            l1 = l2 ;
+            while (l1 != null) {
+                sum = carry + l1.val ;
+                if (sum >= 10) {
+                    carry = sum / 10 ;
+                    sum = sum % 10 ;
+
+                }
+                else carry = 0 ;
+                l1.val = sum ;
+                prev = l1 ;
+                l1 = l1.next ;
+            }
+
+        }
+
+        if (l1 != null && l2 == null) {
+            while (l1 != null) {
+                sum = carry + l1.val ;
+                if (sum >= 10) {
+                    carry = sum / 10 ;
+                    sum = sum % 10 ;
+
+                }
+                else carry = 0 ;
+                prev = l1 ;
+                l1.val = sum ;
+                l1 = l1.next ;
+            }
+        }
+        if (carry > 0) {
+            l1 = new ListNode () ;
+            l1.val = carry ;
+            prev.next = l1 ;
+        }
+        return head ;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Random rand = new Random() ;
+        int upperbound = 100 ;
+        LinkedList ll = new LinkedList() ;
+        int count = 10 ;
+        for (int i = 0 ; i <  count ; i++) {
+            int val = rand.nextInt(upperbound) % 10 ;
+            if (i > (count/2) ) {
+                while (val == 0)
+                    val = rand.nextInt(upperbound) % 10 ;
+                val = 9 ;
+            }
+            ll.insertNode(val);
+        }
+        ll.printll(ll.getHead());
+        LinkedList l2 = new LinkedList() ;
+        count = 6 ;
+        for (int i = 0 ; i <  6 ; i++) {
+            int val = rand.nextInt(upperbound) % 10 ;
+            if (i == (count -1) ) {
+                while (val == 0)
+                    val = rand.nextInt(upperbound) % 10 ;
+            }
+            l2.insertNode(val);
+        }
+        l2.printll(l2.getHead());
+        l2.printll(addTwoNumbers(ll.getHead(), l2.getHead()));
+
+ /*        ListNode rev = ll.reverseLL() ;
+         ll.printll(rev);
+         ll.printll(ll.middleLL());
+*/
+
+
+    }
+}
--- java/interviews/src/main/java/com/evo/ib/linkedlist/LinkedList.java
+++ java/interviews/src/main/java/com/evo/ib/linkedlist/LinkedList.java
@@ -1,2 +1,81 @@
-package com.evo.ib.linkedlist;public class LinkedList {
+package com.evo.ib.linkedlist;
+
+public class LinkedList {
+    ListNode head ;
+    ListNode cur ;
+
+    public ListNode getHead() {
+        return head;
+    }
+
+    public void setHead(ListNode head) {
+        this.head = head;
+    }
+
+    public ListNode getCur() {
+        return cur;
+    }
+
+    public void setCur(ListNode cur) {
+        this.cur = cur;
+    }
+
+    public LinkedList() {
+        head = null ;
+        cur = null ;
+    }
+
+    public void insertNode (int data) {
+        ListNode listNode = new ListNode() ;
+        listNode.setVal(data);
+        listNode.setNext(null);
+        if (head == null) {
+            head = listNode;
+            cur = head ;
+            return ;
+        }
+        cur.setNext(listNode);
+        cur = cur.getNext();
+    }
+
+    public void printll (ListNode p) {
+        while (p != null) {
+            System.out.print (p.getVal() + "--->") ;
+            p = p.getNext() ;
+        }
+        System.out.println("null") ;
+    }
+    // This reverses the existing list
+    public ListNode reverseLL () {
+        ListNode rev = getHead() ;
+
+        if (rev == null) return null ;
+        ListNode prev = null ;
+        ListNode next = rev.getNext() ;
+        while (rev != null ) {
+            rev.setNext(prev); ;
+            prev = rev ;
+            rev = next ;
+            if (rev != null)
+                next = rev.getNext() ;
+        }
+        setHead(prev);
+        return prev ;
+    }
+
+    public ListNode middleLL () {
+        ListNode slowListNode = getHead() ;
+        ListNode fastListNode = getHead() ;
+        while (fastListNode != null) {
+
+            fastListNode = fastListNode.getNext() ;
+            if (fastListNode != null) {
+                fastListNode = fastListNode.getNext() ;
+            }
+            else
+                break;
+            slowListNode = slowListNode.getNext() ;
+        }
+        return slowListNode;
+    }
 }
