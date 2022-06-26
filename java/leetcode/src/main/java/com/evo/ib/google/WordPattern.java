77b1edf50efa6eb4cc94c6b8f4316525d4752e78
arumugam-ramasamy <sundar_hariharane@yahoo.com>
2022-06-26 15:56:42 -0700

    google array problems

--- java/leetcode/src/main/java/com/evo/ib/google/WordPattern.java
+++ java/leetcode/src/main/java/com/evo/ib/google/WordPattern.java
@@ -1,18 +1,61 @@
 package com.evo.ib.google;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class WordPattern {
 
     public static boolean wordPattern (String words, String pattern) {
-
+        if (words == null && pattern == null) return true ;
+        if (words.length() == 0 && pattern.length() == 0) return true ;
+        if (words == null || words.length() == 0) return false ;
+        if (pattern == null || pattern.length() == 0) return false ;
         String[] pats = pattern.split("") ;
         String[] word = words.split(" ") ;
+        if (word.length != pats.length) return false ;
         Map <String, String> patword = new HashMap<>() ;
-        for (int i = 0, j = 0 ; i < word.length ; i++) {
-           // if (patword.containsKey())
+        for (int i = 0, j = 0 ; i < pats.length ; i++, j++) {
+           if (patword.containsKey(pats[i])) {
+               if (word[j].equals(patword.get(pats[i])) == false) {
+                   return false ;
+               }
+           }
+           else {
+               patword.put(pats[i], word[j]) ;
+           }
         }
         return true ;
+    }
+
+    public static Set<String> wordPattern2(String words, String pattern) {
+        if (words == null && pattern == null) return null ;
+        if (words.length() == 0 && pattern.length() == 0) return null ;
+        if (words == null || words.length() == 0) return null ;
+        if (pattern == null || pattern.length() == 0) return null ;
+        Set<String> res = new HashSet<>() ;
+        Map <Character, Integer> patmap = new HashMap<>() ;
+        int i = 0 ;
+        for (Character ch : pattern.toCharArray()) {
+            if (patmap.containsKey(ch) == false) {
+                patmap.put(ch, i) ;
+                i++ ;
+            }
+        }
+        String [] word = words.split(" ") ;
+        for (String each : word) {
+            if (each.length() != pattern.length()) continue ;
+
+        }
+        return res ;
+    }
+
+    public static void main(String[] args) {
+        String pattern = "abba" ;
+        String words = "cat dog dog cat" ;
+        String newws = "cat dog dog lion" ;
+        System.out.println(wordPattern(words, pattern));
+        System.out.println(wordPattern(newws, pattern));
     }
 }
