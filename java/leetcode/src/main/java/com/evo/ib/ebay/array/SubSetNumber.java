9d2d6b65b9e4bfbd78d448e9364ca043b81086d8
arumugam-ramasamy <sundar_hariharane@yahoo.com>
2022-09-23 23:31:16 -0700

    leet code top problems

---
+++ java/leetcode/src/main/java/com/evo/ib/ebay/array/SubSetNumber.java
@@ -0,0 +1,9 @@
+package com.evo.ib.ebay.array;
+
+import java.util.List;
+
+public class SubSetNumber {
+    public static List<List<Integer>> subsetWithNumber(int []arr, int k) {
+        return null ;
+    }
+}
--- java/leetcode/src/main/java/com/evo/ib/ebay/bt/BottomView.java
+++ java/leetcode/src/main/java/com/evo/ib/ebay/bt/BottomView.java
@@ -2,17 +2,22 @@ package com.evo.ib.ebay.bt;
 
 
 import com.evo.ib.bt.BinaryTree;
+import com.evo.ib.tree.TreeNode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BottomView {
 
-    public static List<Integer> bottomView (BinaryTree tree){
+    public static List<Integer> bottomView (TreeNode tree){
         List <Integer> list = new ArrayList<>() ;
         Map<Integer, List<Integer>> bottomView = new HashMap<>() ;
+        if (tree == null) return null ;
+        Map <Integer, List<Integer>> btView = new HashMap<>() ;
+        List<Integer> valSet = new ArrayList<>() ;
+        valSet.add(tree.val) ;
+        btView.put(0, valSet)  ;
+        Queue<TreeNode> treelist = new LinkedList<>() ;
+        treelist.add(tree) ;
 
         return list ;
     }
